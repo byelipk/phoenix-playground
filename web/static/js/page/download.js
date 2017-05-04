1e0cdfef76e8433093699a0ce386eb5a6df4cdfb
byelipk <byelipk@gmail.com>
2017-05-04 09:39:40 -0400

    Add workers to assets/js

--- web/static/js/page/download.js
+++ web/static/js/page/download.js
@@ -4,7 +4,8 @@ export var Page = {
   run: function(path) {
     PDFJS.disableWorker = true;
 
-    PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+    // PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+    PDFJS.workerSrc = '/js/pdf.worker.js';
 
     PDFJS.getDocument(path)
       .then((pdf) => {
