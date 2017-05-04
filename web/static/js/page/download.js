dade1a2f5b4675f7e3db0d743709f8a39df65ed4
byelipk <byelipk@gmail.com>
2017-05-04 09:44:20 -0400

    Render PDF on canvas

--- web/static/js/page/download.js
+++ web/static/js/page/download.js
@@ -1,6 +1,6 @@
 import { PDFJS } from "pdfjs-dist";
 
-export var Page = {
+export let Page = {
   run: function(path) {
     PDFJS.disableWorker = true;
 
@@ -9,7 +9,29 @@ export var Page = {
 
     PDFJS.getDocument(path)
       .then((pdf) => {
-        console.log(pdf);
+
+        pdf.getPage(1).then((page) => {
+          console.log("Page loaded!");
+
+          let scale = 1.5;
+          let viewport = page.getViewport(scale);
+
+          // Prepare canvas using PDF page dimensions
+          let canvas = document.getElementById('the-canvas');
+          let context = canvas.getContext('2d');
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+
+          // Render PDF page into canvas context
+          let renderContext = {
+            canvasContext: context,
+            viewport: viewport
+          };
+          let renderTask = page.render(renderContext);
+          renderTask.then(function () {
+            console.log('Page rendered');
+          });
+        });
       })
       .catch((reason) => {
         console.error(reason);
